346ae405a582f7ba7fe7066ebcc6aab32f9473be
Pablo Matheis <pablomatheis@Pablos-MacBook-Air.local>
2024-04-11 17:17:28 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,429 @@
+let resume = `# 蔡嘉琳
+137xxxxxxxx | caijialinxx@foxmail.com | 北京/深圳/远程
+Github: https://github.com/Caijialinxx
+Blog: https://caijialinxx.github.io
+4年前端开发
+
+## 专业技能
+- 近4年 React 及其相关库的开发经验，理解 Hooks 、虚拟 DOM 、 Diff 算法等原理。可使用 React Native / Vue 等框架开发。
+- 良好的编码习惯，如开发前做方案设计，注重组件化及模块化，擅长并严格执行 CodeReview 等。有较好的问题分析、解决能力。
+- 熟练使用 HTML5 / CSS3 / JavaScript ，常用 TypeScript 进行开发。能够高保真还原设计稿。
+- 擅用 Git 命令进行代码仓库协同⼯作，如处理代码冲突、提交管理、分支管理等。
+- Webpack 构建项⽬的经验，了解常⽤的资源管理、插件、缓存等的使⽤。
+
+## 工作经历
+### 中国平安人寿保险股份有限公司
+2021/3 - 至今
+- 职位：高级前端开发
+- ⼯作内容：主要负责北斗可视化平台的相关工作，具体如下：
+    1. 负责项目前端开发工作，还负责整个版本迭代周期的全流程把控，如需求排期、设计评审、前端代码评审、交付质量保障、风险管理、发布上线等；
+    2. 参与项目的需求规划、分析及评估，根据实际接收到的业务反馈及平台自身定位，协调需求内容和优先级；
+    3. 参与项目的运营和推广，为业务使用提供技术答疑及支持。针对重要业务方建立一对一专属的服务沟通群，积极响应需求，增强用户黏性；辅助运营同事丰富帮助文档、规范答疑话术等。
+
+### 中国平安人寿保险股份有限公司-法本⼈⼒外包
+2020/5 - 2021/3
+- 职位：前端开发（含移动端和 PC 端）
+- ⼯作内容：在捷报 PC 项⽬中担任 Vue Web 开发，负责代码审核及脚⼿架的升级等工作，开展代码优化分享会。在捷报 App 项⽬中担任 React Native 开发，其中负责部分公共组件的开发。独立负责捷报 CMS 前端开发，参与技术侧的需求分析及评估。
+
+### 深圳市亿兆互联技术有限公司
+2020/3 - 2020/4
+- 职位：Web前端开发
+- 工作内容：使用 Vue 全家桶开发和维护智能照明控制系统，独立承担太阳能智能灯控系统中高德地图模块的开发。快速定位和解决 Bug ，针对可优化的地方主动提供优化方案并执行。
+
+### 深圳市芥菜种科技有限公司
+2019/2 - 2019/5
+- 职位：Web前端开发
+- 工作内容：使用 React 框架开发智能英语学习平台，状态管理库使用 Mobx 。负责中后台 1.1 版本的所有更新和优化；To C 系统中负责 UI 组件的开发供前端调用，以及数学学习模块的更新维护。
+
+## 项目经验
+### 北斗可视化平台
+（公司项目，无预览链接）
+- 关键词：\`React Hooks\`、\`ECharts\`、\`TypeScript\`、\`Redux\`、\`Rxjs\`、\`Less\` 、\`专利\`、\`BI平台\`  
+- 描述：是一个大数据高效分析与展现的平台，通过对数据源的连接、数据集的创建，可对数据进行即时分析与查询，并通过拖拽的方式将数据可视化地呈现为各种图表。负责的工作成果如下（数据统计至2023-03）：
+    1. 开发工作中，独立负责的支持多层嵌套的 Tab 方案获得职务专利——*基于人工智能的网格布局方法、装置、设备及存储介质（公开号：CN114637506A）*；
+    2. 负责前端代码评审，相较未引⼊代码评审前，测试/⽣产阶段平均每个版本减少 4 个 Bug ，约节省测试⼈⼒ 2 ⼈天，并借此帮助 3 个初级开发提⾼了编码能⼒并建⽴规范意识，使前端项⽬质量及可维护性提升；
+    3. 负责前端组件化、模块化，编写了 20+ 个逻辑处理函数供具有相同逻辑的函数/组件/模块进行调用，其中一个函数最多被 14 个组件引入使用，大大减少了维护成本，节省超 60% 人力；
+    4. 负责定位、评估遗留 Bug 并提供修复方案（已修复 51 个），主导前端安全加固，如接口入参加密、签名、页面权限控制等，使平台安全、稳定运营——⽀持⽤户快速开发和迭代报表，相⽐定制化开发模式，每张报表从开发到上线时间缩短了 55% ~ 78% ，提效 122% ~ 344% ，节省约 76% ⼈⼒；报表管理的⽉活⽤户约 1300 ⼈，活跃率约 15% ，共制作报表 1800+ 张，⽉均访问 20000+ 次，⽉均下载 6000+ 次；邮件订阅中活跃任务 126 份，占任务总数 58% ，共服务单⽤户 1661 ⼈、邮件组 35 个。
+
+### CUI
+[预览](https://caijialinxx.github.io/ui-demo/)
+- 关键词：\`UI 组件库\`、\`React Hooks\`、\`TypeScript\`、\`npm\`、\`webpack\`、\`CircleCi\`
+- 描述：个人独立开发的 React UI 组件库，使用 React Hooks + TypeScript 开发，并已发布到 npm 中。由此掌握了 webpack 打包、 npm 发布流程、 CircleCi 自动化持续集成等。其中遇到的一个问题记录在了博客中——[从 npm 引入 React Hooks 轮子库报错 Minified React error#321 的解决方法](https://caijialinxx.github.io/2019/11/20/fix-react-error-321-by-webpack-externals/)。
+
+### 简历生成器
+[预览](https://caijialinxx.github.io/animating-resume/)
+- 关键词：\`JavaScript\`、\`Promise\`、\`变速\`、\`过渡动画\`、\`盒阴影\`
+- 描述：该项目实现将一个 Markdown 简历转成可下载的 PDF 简历。使用原生 JS 开发， Promise 处理异步链式操作（各个步骤的跳转衔接），利用 CSS3 实现过渡、动画、阴影和弹性布局等。
+
+### 涂鸦板
+[预览](https://caijialinxx.github.io/Canvas/)
+- 关键词：\`HTML5\`、\`Canvas\`、\`ES6\`、\`JavaScript\`
+- 描述：使用 HTML5 的 Canvas 元素及其相关 API ，实现支持多指同时画图（可选色/粗细）、橡皮擦、一键清空、保存至本地的功能。
+
+## 教育经历
+### 广东第二师范学院
+2014/9 - 2018/6
+- 专业学历：计算机科学与技术 大学本科
+- GPA：3.4 / 4.0
+- 荣誉：校二、三奖学金、优秀学生（2014-2015、2016-2017共两学年）、优秀学生干部（2014-2015、2016-2017共两学年）
+`
+
+let code_ready = `/*
+ * 嗨，我是蔡嘉琳。
+ * 我想用代码给你展示一下我的个人简历～
+ * 灵感来自：http://strml.net
+ * 
+ * 那么，我要开始啦...
+ */
+
+/* 添加一个过渡，使变化看起来更柔和 */
+* {
+  transition: all 0.3s;
+}
+/* 加个背景色 */
+body {
+  background: #455A64;
+}
+/* 给代码加个边框 */
+#code_body {
+  width: 100%;
+  border: 1px solid #CFD8DC;
+  background: #CFD8DC;
+  padding: 16px;
+  overflow: auto;
+}
+
+/* 让代码高亮起来 */
+.token.comment, .token.punctuation {
+  color: #757575;
+}
+.token.selector {
+  color: #007400;
+}
+.token.property {
+  color: #cf1f1f;
+}
+
+/* 
+ * 给它们来个特效吧！
+ * 让它们在被编辑的时候产生“呼吸”的效果
+ */
+.breathe {
+  animation: breathe 3s ease 0s infinite;
+}
+
+
+/* 现在开始写简历啦！ */
+
+/* 让代码框腾出一些的位置给我的简历框 */
+#options {
+  width: 32%;
+}
+/* 准备一个白板 */
+#paper {
+  flex: 1;
+  background: #fdfdfd;
+  margin: 16px;
+  overflow: auto;
+  white-space: pre-wrap;
+}
+
+/* 接下来，请看右边 */
+
+`
+
+let code_marked = `
+/* 
+ * 接下来我要使用一个优秀的库 marked.js
+ * 来使我的简历变成一篇 Markdown
+ * (https://github.com/markedjs/marked)
+ */
+`
+
+let code_beautify_resume = `
+/* 调整我的简历，让它变得再好看一点 */
+#paper {
+  white-space: unset;
+  padding: 0 32px;
+  font-size: 14px;
+}
+#paper a {
+  color: #455A64;
+}
+#paper ul, #paper ol {
+  padding-left: 20px;
+}
+/* 让每个模块上下间隔明显一点 */
+#paper > div {
+  margin: 24px 0;
+}
+#paper em {
+  color: peru;
+  font-style: normal;
+}
+
+/* 接下来是精细的调整 */
+
+/* 首先，调整我的“基础信息”板块 */
+#paper h1 {
+  font-size: 22px;
+  margin-bottom: 10px;
+}
+#information {
+  text-align: center;
+}
+#information p {
+  line-height: 24px;
+  white-space: pre-wrap;
+}
+
+/* 然后是详细介绍模块 */
+#paper h2 {
+  font-size: 16px;
+  border-bottom: 1px solid #455A64;
+  padding-bottom: 6px;
+  margin-bottom: 6px;
+}
+#paper h3 {
+  display: inline-block;
+  font-size: 14px;
+  margin: 4px 0;
+}
+#skills > ul ul {
+  margin: 4px 0;
+}
+#skills li, #works li, #education li {
+  margin-bottom: 4px;
+}
+#jobs > .single {
+  margin-bottom: 6px;
+}
+
+/* 调整“实习经历”和“教育经历”中时间的位置 */
+#jobs > .single, #education > .single, #works > .single {
+  position: relative;
+}
+#jobs p, #education p, #works p {
+  position: absolute;
+  right: 0px;
+  top: 4px;
+}
+
+
+/* 接下来添加个头像吧！ */
+`
+
+let code_photo = `
+/* 把我的头像放在个人信息里 */
+#information {
+  position: relative;
+}
+#information .avatar {
+  width: 100px;
+  position: absolute;
+  right: 0px;
+  top: 0px;
+}
+/* 让多余的照片隐藏吧~ */
+#information {
+  overflow: hidden;
+}
+
+/* 
+ * 好啦，我的头像出来啦~
+ * 期待这张面孔出现在您的面试中
+ * 
+ *   —— 蔡嘉琳 2019/01 *^_^*
+ */
+`
+
+var resumeInputTimeoutID, codeInputTimeoutID
+writeCode(code_ready, '').then(() => {
+  createResume().then(() => {
+    $('#skip_inputResume').remove()
+    adjustResume().then(() => {
+      addAndAdjustAvatar().then(showDownloadButton)
+    }, () => {
+      showFinalResult()
+    })
+  })
+})
+
+var speedCode = 1, duration = 80
+$('#speed_btn').click(() => {
+  speedCode += 1
+  if (speedCode > 3) {
+    speedCode = 1
+  }
+  switch (speedCode) {
+    case 1:
+      speed_status.innerText = '🚲'
+      description.innerText = '开小车车快点吧~'
+      duration = 70
+      break
+    case 2:
+      speed_status.innerText = '🚗'
+      description.innerText = '太废话了，我要最快！'
+      duration = 30
+      break
+    case 3:
+      speed_status.innerText = '🚀'
+      description.innerText = '我眼花了！慢一点~'
+      duration = 0
+      break
+    default:
+      return 0
+  }
+})
+
+function writeCode(code, origin) {
+  let n = 1
+  return new Promise(resolve => {
+    codeInputTimeoutID = setTimeout(write, duration)
+    function write() {
+      code_body.innerHTML = Prism.highlight(origin + code.substr(0, n), Prism.languages.css, 'css');
+      code_style.innerHTML = origin + code.substr(0, n)
+      code_body.scrollTop = code_body.scrollHeight
+      n++
+      if (n === code.length) {
+        window.clearTimeout(codeInputTimeoutID)
+        resolve.call(undefined)
+      } else {
+        window.clearTimeout(codeInputTimeoutID)
+        codeInputTimeoutID = setTimeout(write, duration)
+      }
+    }
+  })
+}
+function writeResume() {
+  let n = 1
+  return new Promise(resolve => {
+    resumeInputTimeoutID = setTimeout(write, duration)
+    function write () {
+      paper.innerHTML = resume.substr(0, n)
+      paper.scrollTop = paper.scrollHeight
+      n++
+      if (n === resume.length) {
+        window.clearTimeout(resumeInputTimeoutID)
+        resolve.call(undefined)
+      } else {
+        window.clearTimeout(resumeInputTimeoutID)
+        resumeInputTimeoutID = setTimeout(write, duration)
+      }
+    }
+  })
+}
+
+function createResume() {
+  return new Promise(resolve => {
+    $('#paper').addClass('breathe')
+    $('#code_body').removeClass('breathe')
+    writeResume().then(resolve)
+    setTimeout(() => {
+      $('#skip_inputResume').show()
+      $('#skip_inputResume').click(() => {
+        skipResumeInput().then(resolve)
+      })
+    }, 1000)
+  })
+}
+function adjustResume() {
+  return new Promise((resolve, reject) => {
+    $('#code_body').addClass('breathe')
+    $('#paper').removeClass('breathe')
+    writeCode(code_marked, code_ready).then(() => {
+      structureResume()
+      $('#paper')[0].scrollTop = 0
+      writeCode(code_beautify_resume, code_ready + code_marked).then(resolve)
+      setTimeout(() => {
+        $('#skip_all').show()
+        $('#skip_all').click(() => {
+          skipAll().then(reject)
+        })
+      }, 1000)
+    })
+  })
+}
+function structureResume() {
+  $('#paper')[0].innerHTML = marked(resume)
+  $('#paper').prepend('<div id="information"></div>', '<div id="skills"></div>', '<div id="jobs"></div>', '<div id="works"></div>', '<div id="education"></div>')
+  
+  // 「基本信息」
+  $('#information').append($('h1'), $('p').first())
+
+  // 「专业技能」
+  const skills_header = $('h2:contains("技能")')
+  $('#skills').append(skills_header, skills_header.next('ul')[0])
+
+  // 「教育经历」
+  $('#education').append($('h2:contains("教育")'), $('<div class="single"></div>').append($('h3:contains("学院")'), $('p').last(), $('ul').last()))
+
+  // 「工作经历」
+  $('#jobs').append($('h2:contains("工作")'))
+  $('h3:contains("公司")').each((index, element) => {
+    $('<div class="single"></div>')
+      .append($(element), $(element).next('p')[0], $(element).next('p').next('ul')[0])
+      .appendTo('#jobs')
+  })
+
+  // 「项目经验」模块
+  $('#works').append($('h2:contains("项目")'))
+  const project_title_array = $('#paper > h3')    // 剩余的h3就是项目名
+  project_title_array.each((index, element) => {
+    $('<div class="single"></div>')
+      .append($(element), $(element).next('p')[0], $(element).next('p').next('ul')[0])
+      .appendTo('#works')
+  })
+
+  // 「小程序」
+  // $('a[href="./images/qrcode.png"]').addClass('qrcode-trigger').append($('img.qrcode'))
+}
+function addAndAdjustAvatar() {
+  return new Promise(resolve => {
+    $('#information').append($('img.avatar'))
+    writeCode(code_photo, code_ready + code_marked + code_beautify_resume).then(() => {
+      $('#paper').addClass('breathe')
+      $('#code_body').removeClass('breathe')
+      $('.skipper').remove()
+      setTimeout(resolve, 1000)
+    })
+  })
+}
+function showDownloadButton() {
+  $('#options').css({ 'width': '23%' })
+  $('a.downloadResume').addClass('show')
+  $('#options').append($('a.downloadResume'))
+}
+
+function skipResumeInput() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      window.clearTimeout(resumeInputTimeoutID)
+      paper.innerHTML = resume
+      paper.scrollTop = paper.scrollHeight
+      resolve()
+      $('#skip_inputResume').remove()
+    }, 0)
+  })
+}
+function skipAll() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      window.clearTimeout(codeInputTimeoutID)
+      resolve()
+      $('.skipper').remove()
+    }, 0)
+  })
+}
+function showFinalResult() {
+  $('#paper').addClass('breathe')
+  $('#code_body').removeClass('breathe')
+  $('#information').append($('img.avatar'))
+  code_body.innerHTML = Prism.highlight(code_ready + code_marked + code_beautify_resume + code_photo, Prism.languages.css, 'css');
+  code_style.innerHTML = code_ready + code_marked + code_beautify_resume + code_photo
+  code_body.scrollTop = code_body.scrollHeight
+  window.clearTimeout(codeInputTimeoutID)
+  $('#options').css({ 'width': '23%' })
+  $('a.downloadResume').addClass('show')
+  $('#options').append($('a.downloadResume'))
+}
